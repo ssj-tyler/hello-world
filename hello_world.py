a82da3f636264a9216ec7ac2f3986d71a2b50fbc
J. Tyler Phillips <127756532+ssj-tyler@users.noreply.github.com>
2023-04-04 13:07:24 +0000

    Added hello_world to repository

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+HW = 'Hello World'
+
+#Prints 'Hello World' from the saved variable
+print(HW)
+print()
+
+#This is the traditional way to print 'Hello World' by adding strings
+print('This is one way to print ' + HW + '.')
+print()
+
+#This is a new method to print 'Hello World' that came out with 3.6 iirc
+print(f'This is a new and improved way to print {HW}.')
+print()
